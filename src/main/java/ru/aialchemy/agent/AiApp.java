5c29ab7be9f97e5d312b8e4b3a3c2e3663f68f3a
Olga Maksimochkina <maksimochkinaol@gmail.com>
2025-10-23 12:12:27 +0300

    base options added

---
+++ src/main/java/ru/aialchemy/agent/AiApp.java
@@ -0,0 +1,15 @@
+package ru.aialchemy.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class AiApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AiApp.class);
+    }
+}
