55454716ad9397bcc5e250b58e5e04ccb7382ac9
Olga Maksimochkina <maksimochkinaol@gmail.com>
2025-10-27 17:37:13 +0300

    The controller edited

--- src/main/java/ru/aialchemy/agent/controller/AiController.java
+++ src/main/java/ru/aialchemy/agent/controller/AiController.java
@@ -3,12 +3,10 @@ package ru.aialchemy.agent.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.aialchemy.agent.models.UserRq;
 import ru.aialchemy.agent.service.GigaChatSrv;
@@ -27,18 +25,17 @@ public class AiController {
         this.validateRqSrv = validateRqSrv;
     }
 
-    @PostMapping("/documents/edit")
-    public ResponseEntity<String> editDoc(RequestEntity<UserRq> request, @RequestParam("file") MultipartFile file) {
+    @PostMapping(value = "/documents/edit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> editDoc(@RequestParam(name = "userRq") RequestEntity<UserRq> request, @RequestPart("file") MultipartFile file) {
         var body = request.getBody();
         log.info("Получен запрос пользователя: {} на работу с файлом {}", body, file.getOriginalFilename());
 
         var wordDocumentContent = validateRqSrv.validate(file);
-        var errorMessage = wordDocumentContent.errorMessage();
-        if (errorMessage != null) {
-            return ResponseEntity.badRequest().body(errorMessage);
+        if (wordDocumentContent.errorMessage() != null) {
+            return ResponseEntity.badRequest().body(wordDocumentContent.errorMessage());
         }
 
-        var result = "Получен корректный файл для анализа";
+        var result = "Получен корректный файл для анализа:\n\n"+wordDocumentContent.content();
 //                gigaChatSrv.editText(body);
         return ResponseEntity.ok(result);
     }
